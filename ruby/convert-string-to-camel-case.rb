98cbe3bbe01cfb2d942946bfb01368dc65aaa81b
grailwilly <grailjwi11y@gmail.com>
2022-11-21 09:39:02 +0800

    test amend

---
+++ ruby/convert-string-to-camel-case.rb
@@ -0,0 +1,14 @@
+# Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+# Examples
+# "the-stealth-warrior" gets converted to "theStealthWarrior"
+# "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+def to_camel_case(str)
+ test git commit ammend
+end
+
+# Test.assert_equals(to_camel_case(''), '', "An empty string was provided but not returned")
+# Test.assert_equals(to_camel_case("the_stealth_warrior"), "theStealthWarrior", "to_camel_case('the_stealth_warrior') did not return correct value")
+# Test.assert_equals(to_camel_case("The-Stealth-Warrior"), "TheStealthWarrior", "to_camel_case('The-Stealth-Warrior') did not return correct value")
+# Test.assert_equals(to_camel_case("A-B-C"), "ABC", "to_camel_case('A-B-C') did not return correct value")
